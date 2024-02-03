a540161799476e22b290bc292d708bdea9128aaa
Hanna Saurytskaya <anna.kassy.s@gmail.com>
2024-02-03 12:24:00 +0300

    Add Feign Client classes.

---
+++ src/main/java/com/team/syberry/feign/INationalBankApiClient.java
@@ -0,0 +1,30 @@
+package com.team.syberry.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@FeignClient(name = "NationalBankApiClient", url = "https://api.nbrb.by")
+public interface INationalBankApiClient {
+
+    @GetMapping("/exrates/currencies")
+    List<CurrencyDto> getCurrenciesList();
+
+    @GetMapping("/exrates/rates/{cur_id}")
+    public CurrencyRateDto getCurrencyRate(@PathVariable String currencyCode,
+                                           @RequestParam LocalDate date);
+
+    @GetMapping("/exrates/rates/dynamics/{cur_id}")
+    public List<CurrencyRateDto> getCurrencyRateForPeriod(@PathVariable String currencyCode,
+                                                          @RequestParam LocalDate from,
+                                                          @RequestParam  LocalDate to);
+
+    @GetMapping("/exrates/rates/{cur_id}")
+    public StatisticsDto getStatistics(@PathVariable String currencyCode,
+                                       @RequestParam LocalDate from,
+                                       @RequestParam LocalDate to);
+}
