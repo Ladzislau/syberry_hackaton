373ba16389959642d8eb974da6dafd06466400a1
vlad <mainvgd@gmail.com>
2024-02-03 10:09:38 +0300

    init commit

---
+++ src/main/java/com/team/syberry/SyberryApplication.java
@@ -0,0 +1,13 @@
+package com.team.syberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyberryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyberryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/syberry/SyberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.syberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyberryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
