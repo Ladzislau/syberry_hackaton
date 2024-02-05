a59470d554c20e65ea0f96e24b9355b95fd769fe
vlad <mainvgd@gmail.com>
2024-02-05 11:10:40 +0300

    обновлены параметры запросов

--- src/main/java/com/team/syberry/feign/INationalBankApiClient.java
+++ src/main/java/com/team/syberry/feign/INationalBankApiClient.java
@@ -2,6 +2,7 @@ package com.team.syberry.feign;
 
 import com.team.syberry.domain.nationalbank.*;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
